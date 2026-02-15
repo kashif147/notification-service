dfef9c5851a407f30bff5932c28300f365e19ed6
Kashif <kashif147@gmail.com>
2026-02-15 08:57:43 +0000

    change batch.events to portal.events for testing

--- rabbitMQ/index.js
+++ rabbitMQ/index.js
@@ -92,7 +92,7 @@ async function setupConsumers() {
     });
 
     // 2. Bind queue to exchange + routing keys
-    await consumer.bindQueue(NOTIFICATION_QUEUE, "batch.events", [
+    await consumer.bindQueue(NOTIFICATION_QUEUE, "portal.events", [
       "batch.completed",
     ]);
 
